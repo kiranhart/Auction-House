ab2ff169a495826876ee1132291a4cea68d47c29
Kiran Hart <kiran.hart@live.com>
2022-08-25 08:18:38 -0400

    if getFriendlyNumber is unable to format correctly, default to normal number format
    Took 21 seconds

--- src/main/java/ca/tweetzy/auctionhouse/api/AuctionAPI.java
+++ src/main/java/ca/tweetzy/auctionhouse/api/AuctionAPI.java
@@ -85,12 +85,17 @@ public class AuctionAPI {
 		String suffix = " KMBTQPEZY";
 		String formattedNumber = "";
 
-		NumberFormat formatter = new DecimalFormat("#,###.#");
-		power = (int) StrictMath.log10(value);
-		value = value / (Math.pow(10, (power / 3) * 3));
-		formattedNumber = formatter.format(value);
+		try {
+			NumberFormat formatter = new DecimalFormat("#,###.#");
+			power = (int) StrictMath.log10(value);
+			value = value / (Math.pow(10, (power / 3) * 3));
+			formattedNumber = formatter.format(value);
 
-		formattedNumber = formattedNumber + suffix.charAt(power / 3);
+			formattedNumber = formattedNumber + suffix.charAt(power / 3);
+
+		} catch (StringIndexOutOfBoundsException e) {
+			return formatNumber(value);
+		}
 
 		return formattedNumber.length() > 4 ? formattedNumber.replaceAll("\\.[0-9]+", "") : formattedNumber;
 	}
