df128270a746fc894ab97a35d1a79e1647f79d6c
Kiran Hart <kiran.hart@live.com>
2023-07-02 18:29:56 -0400

    🪶 fix weird lore "dupe" in gui when removing min item prices
    Took 2 minutes

--- src/main/java/ca/tweetzy/auctionhouse/guis/GUIMinItemPrices.java
+++ src/main/java/ca/tweetzy/auctionhouse/guis/GUIMinItemPrices.java
@@ -64,10 +64,10 @@ public final class GUIMinItemPrices extends AbstractPlaceholderGui {
 
 		for (MinItemPrice minItemPrice : data) {
 
-			final List<String> lore = AuctionAPI.getInstance().getItemLore(minItemPrice.getItemStack());
+			final List<String> lore = AuctionAPI.getInstance().getItemLore(minItemPrice.getItemStack().clone());
 			lore.addAll(Settings.GUI_MIN_ITEM_PRICES_LORE.getStringList());
 
-			setButton(slot++, ConfigurationItemHelper.createConfigurationItem(this.player, minItemPrice.getItemStack(), AuctionAPI.getInstance().getItemName(minItemPrice.getItemStack()), lore, new HashMap<String, Object>() {{
+			setButton(slot++, ConfigurationItemHelper.createConfigurationItem(this.player, minItemPrice.getItemStack().clone(), AuctionAPI.getInstance().getItemName(minItemPrice.getItemStack()), lore, new HashMap<String, Object>() {{
 				put("%price%", AuctionAPI.getInstance().formatNumber(minItemPrice.getPrice()));
 			}}), click -> {
 				AuctionHouse.getInstance().getDataManager().deleteMinItemPrice(Collections.singletonList(minItemPrice.getUuid()));
