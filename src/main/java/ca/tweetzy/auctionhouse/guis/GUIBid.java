20fe5f7798e9e5100d95bd85513aeff2860ec71a
Kiran Hart <kiran.hart@live.com>
2023-08-08 18:07:33 -0400

    🔄️ call bid event from custom amount w/o confirm synchronously
    Took 7 minutes

--- src/main/java/ca/tweetzy/auctionhouse/guis/GUIBid.java
+++ src/main/java/ca/tweetzy/auctionhouse/guis/GUIBid.java
@@ -156,7 +156,7 @@ public class GUIBid extends AbstractPlaceholderGui {
 					OfflinePlayer owner = Bukkit.getOfflinePlayer(auctionItem.getOwner());
 
 					AuctionBidEvent auctionBidEvent = new AuctionBidEvent(e.player, auctionItem, newBiddingAmount);
-					Bukkit.getServer().getPluginManager().callEvent(auctionBidEvent);
+					Bukkit.getServer().getScheduler().runTask(AuctionHouse.getInstance(), () -> Bukkit.getServer().getPluginManager().callEvent(auctionBidEvent));
 					if (auctionBidEvent.isCancelled()) return true;
 
 					if (Settings.BIDDING_TAKES_MONEY.getBoolean()) {
