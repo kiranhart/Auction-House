0af9c3e75299875ba3673341b959040a22cea7b8
Kiran Hart <kiran.hart@live.com>
2023-08-11 09:16:17 -0400

    🏷️ adjust variables for transaction view menu
    Took 2 minutes

--- src/main/java/ca/tweetzy/auctionhouse/guis/transaction/GUITransactionView.java
+++ src/main/java/ca/tweetzy/auctionhouse/guis/transaction/GUITransactionView.java
@@ -26,12 +26,10 @@ import ca.tweetzy.auctionhouse.guis.AbstractPlaceholderGui;
 import ca.tweetzy.auctionhouse.helpers.ConfigurationItemHelper;
 import ca.tweetzy.auctionhouse.settings.Settings;
 import ca.tweetzy.auctionhouse.transaction.Transaction;
-import ca.tweetzy.core.gui.GuiUtils;
 import ca.tweetzy.core.utils.TextUtils;
 import org.bukkit.Bukkit;
 
 import java.util.HashMap;
-import java.util.stream.Collectors;
 
 /**
  * The current file has been created by Kiran Hart
@@ -52,11 +50,28 @@ public class GUITransactionView extends AbstractPlaceholderGui {
 		setButton(5, 0, getBackButtonItem(), e -> e.manager.showGUI(e.player, new GUITransactionList(auctionPlayer.getPlayer(), showAll)));
 		setItem(1, 4, transaction.getItem());
 
-		setItem(3, 2, GuiUtils.createButtonItem(AuctionAPI.getInstance().getPlayerHead(Bukkit.getOfflinePlayer(transaction.getSeller()).getName()), TextUtils.formatText(Settings.GUI_TRANSACTION_VIEW_ITEM_SELLER_NAME.getString().replace("%seller_name%", Bukkit.getOfflinePlayer(transaction.getSeller()).getName())),
-				Settings.GUI_TRANSACTION_VIEW_ITEM_SELLER_LORE.getStringList().stream().map(line -> line.replace("%seller_id%", transaction.getSeller().toString())).map(TextUtils::formatText).collect(Collectors.toList())));
 
-		setItem(3, 6, GuiUtils.createButtonItem(AuctionAPI.getInstance().getPlayerHead(Bukkit.getOfflinePlayer(transaction.getBuyer()).getName()), TextUtils.formatText(Settings.GUI_TRANSACTION_VIEW_ITEM_BUYER_NAME.getString().replace("%buyer_name%", Bukkit.getOfflinePlayer(transaction.getBuyer()).getName())),
-				Settings.GUI_TRANSACTION_VIEW_ITEM_BUYER_LORE.getStringList().stream().map(line -> line.replace("%buyer_id%", transaction.getBuyer().toString())).map(TextUtils::formatText).collect(Collectors.toList())));
+		setItem(3, 2, ConfigurationItemHelper.createConfigurationItem(
+				this.player,
+				AuctionAPI.getInstance().getPlayerHead(Bukkit.getOfflinePlayer(transaction.getSeller()).getName()),
+				Settings.GUI_TRANSACTION_VIEW_ITEM_SELLER_NAME.getString(),
+				Settings.GUI_TRANSACTION_VIEW_ITEM_SELLER_LORE.getStringList(), new HashMap<String, Object>() {{
+					put("%transaction_id%", transaction.getId().toString());
+					put("%seller%", Bukkit.getOfflinePlayer(transaction.getSeller()).getName());
+					put("%buyer%", Bukkit.getOfflinePlayer(transaction.getBuyer()).getName());
+					put("%date%", AuctionAPI.getInstance().convertMillisToDate(transaction.getTransactionTime()));
+				}}));
+
+		setItem(3, 6, ConfigurationItemHelper.createConfigurationItem(
+				this.player,
+				AuctionAPI.getInstance().getPlayerHead(Bukkit.getOfflinePlayer(transaction.getSeller()).getName()),
+				Settings.GUI_TRANSACTION_VIEW_ITEM_BUYER_NAME.getString(),
+				Settings.GUI_TRANSACTION_VIEW_ITEM_BUYER_LORE.getStringList(), new HashMap<String, Object>() {{
+					put("%transaction_id%", transaction.getId().toString());
+					put("%seller%", Bukkit.getOfflinePlayer(transaction.getSeller()).getName());
+					put("%buyer%", Bukkit.getOfflinePlayer(transaction.getBuyer()).getName());
+					put("%date%", AuctionAPI.getInstance().convertMillisToDate(transaction.getTransactionTime()));
+				}}));
 
 		setItem(3, 4, ConfigurationItemHelper.createConfigurationItem(this.player, Settings.GUI_TRANSACTION_VIEW_ITEM_INFO_ITEM.getString(), Settings.GUI_TRANSACTION_VIEW_ITEM_INFO_NAME.getString(), Settings.GUI_TRANSACTION_VIEW_ITEM_INFO_LORE.getStringList(), new HashMap<String, Object>() {{
 			put("%transaction_id%", transaction.getId().toString());
