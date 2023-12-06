77af181a7919fb40f1f3a539301521cb6e373f6d
Kiran Hart <kiran.hart@live.com>
2023-12-06 15:26:52 -0500

    ✅ fix search permission for filter menu (Closes #78)
    Took 2 minutes

--- src/main/java/ca/tweetzy/auctionhouse/guis/filter/GUIFilterSelection.java
+++ src/main/java/ca/tweetzy/auctionhouse/guis/filter/GUIFilterSelection.java
@@ -76,7 +76,7 @@ public class GUIFilterSelection extends AbstractPlaceholderGui {
 			setButton(1, 5, ConfigurationItemHelper.createConfigurationItem(this.player, Settings.GUI_FILTER_ITEMS_SEARCH_ITEM.getString(), Settings.GUI_FILTER_ITEMS_SEARCH_NAME.getString(), Settings.GUI_FILTER_ITEMS_SEARCH_LORE.getStringList(), new HashMap<String, Object>() {{
 				put("%filter_search_phrase%", auctionPlayer.getCurrentSearchPhrase());
 			}}), e -> {
-				if (e.player.hasPermission("auctionhouse.cmd.search")) {
+				if (!e.player.hasPermission("auctionhouse.cmd.search")) {
 					AuctionHouse.getInstance().getLocale().getMessage("commands.no_permission").sendPrefixedMessage(e.player);
 					return;
 				}
