d00c26ef4042c34cb9bf9d5700603a94f167d68b
Kiran Hart <kiran.hart@live.com>
2024-10-31 12:28:31 -0400

    🔨 moved admin event to new api pkg
    Took 26 seconds

--- src/main/java/ca/tweetzy/auctionhouse/api/event/AuctionAdminEvent.java
+++ src/main/java/ca/tweetzy/auctionhouse/api/event/AuctionAdminEvent.java
@@ -1,4 +1,31 @@
 package ca.tweetzy.auctionhouse.api.event;
 
-public final class AuctionAdminEvent {
+import ca.tweetzy.auctionhouse.auction.AuctionAdminLog;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+@Getter
+@Setter
+public final class AuctionAdminEvent extends Event implements Cancellable {
+
+	private static final HandlerList handlers = new HandlerList();
+	private boolean cancelled;
+
+	private final AuctionAdminLog auctionAdminLog;
+
+	public AuctionAdminEvent(AuctionAdminLog auctionAdminLog) {
+		this.auctionAdminLog = auctionAdminLog;
+	}
+
+
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
 }
--- src/main/java/ca/tweetzy/auctionhouse/guis/admin/GUIAdminItem.java
+++ src/main/java/ca/tweetzy/auctionhouse/guis/admin/GUIAdminItem.java
@@ -25,7 +25,7 @@ import ca.tweetzy.auctionhouse.auction.AuctionPlayer;
 import ca.tweetzy.auctionhouse.auction.AuctionedItem;
 import ca.tweetzy.auctionhouse.auction.enums.AdminAction;
 import ca.tweetzy.auctionhouse.auction.enums.PaymentReason;
-import ca.tweetzy.auctionhouse.events.AuctionAdminEvent;
+import ca.tweetzy.auctionhouse.api.event.AuctionAdminEvent;
 import ca.tweetzy.auctionhouse.guis.AuctionBaseGUI;
 import ca.tweetzy.auctionhouse.guis.core.GUIAuctionHouse;
 import ca.tweetzy.auctionhouse.settings.Settings;
